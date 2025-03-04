d875274f039bc3b7ea894cce660491b33c915883
Roberto Gomez <roberto.gomez02@globant.com>
2025-03-04 10:54:32 -0500

    fix: small refactor

--- src/test/java/com/rcgomez/tests/SampleTest.java
+++ src/test/java/com/rcgomez/tests/SampleTest.java
@@ -1,11 +1,13 @@
 package com.rcgomez.tests;
 
 import base.BaseTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class SampleTest extends BaseTest {
+   @Parameters({"username", "password"})
    @Test
-   public void test() {
-      logIn("standard_user", "secret_sauce");
+   public void test(String user, String password) {
+      logIn(user, password);
    }
 }
