c1c6364972a4f14e0416bb90b52b3f3e8434157d
Roberto Gomez <roberto.gomez02@globant.com>
2025-03-04 16:27:50 -0500

    fix: small refactor

--- src/main/java/com/rcgomez/pages/InventoryPage.java
+++ src/main/java/com/rcgomez/pages/InventoryPage.java
@@ -2,20 +2,17 @@ package com.rcgomez.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
 public class InventoryPage extends BasePage {
 
-    @FindAll({@FindBy(css = ".btn_inventory")})
+    @FindBy(css = ".btn_inventory")
     List<WebElement> buttonsList;
 
     @FindBy(css = ".shopping_cart_link")
     WebElement shoppingCartButton;
-
-
 
     public InventoryPage(WebDriver driver) {
         super(driver);
